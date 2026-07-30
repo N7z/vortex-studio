fd3a8276595c7c5ba4ecdcffae9aecaca7f6d353
Paulo Junior <paulonp@proton.me>
2026-07-30 11:07:18 -0300

    fix vite dev assets: pin ipv4 host, add react refresh preamble

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         react(),
     ],
     server: {
+        host: '127.0.0.1',
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
